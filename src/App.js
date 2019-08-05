a67cbcca9d5523c2c1d677495db79b4fe07f6fb1
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-08-05 13:40:17 +0100

    react router dom implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -13,8 +13,11 @@ const HatsPage  = () => (
 function App() {
   return (
     <div>
-      <HomePage />
-    
+      <Switch>
+         <Route exact path='/' component={HomePage} />
+         <Route exact path='/hats' component={HatsPage} />
+
+        </Switch>    
     </div>
   );
 }
