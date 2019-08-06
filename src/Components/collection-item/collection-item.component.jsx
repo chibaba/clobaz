f7fd58b1c4962cc4bda54a568bef66c0a4db1149
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-08-06 15:30:24 +0100

    coolection-preview added

--- src/Components/collection-item/collection-item.component.jsx
+++ src/Components/collection-item/collection-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './collection-item.styles.scss';
+import './collection-item.component.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className='collection-item'>
@@ -10,8 +10,8 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
                 backgroundImage:`url(${imageUrl})`
             }} />
                 <div className='collection-footer'>
-                <span className='name'>{{ name}}</span>
-                <span className='price'>{{ price }}</span>
+                <span className='name'>{ name}</span>
+                <span className='price'>{ price }</span>
 
             
         </div>
