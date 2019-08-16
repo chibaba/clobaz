c6ad0a31c5ba7878e0ce8154d3861f44006fb229
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-08-16 16:07:59 +0100

    added firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCAZ0lIYtd8RA891cE7gAyLeZaKfO9lNuo",
+    authDomain: "clobaz-9dcc4.firebaseapp.com",
+    databaseURL: "https://clobaz-9dcc4.firebaseio.com",
+    projectId: "clobaz-9dcc4",
+    storageBucket: "",
+    messagingSenderId: "203881900265",
+    appId: "1:203881900265:web:89a3a73b82042157"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
