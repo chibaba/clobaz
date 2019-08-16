3adaa17056c698c8c78ca06bcd8922f24a6e1191
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-08-16 20:37:17 +0100

    querying data from database

--- src/App.js
+++ src/App.js
@@ -6,20 +6,44 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component'
 import Header from  './Components/header/header.component'; 
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-         <Route exact path='/' component={HomePage} />
-         <Route  path='/shop' component={ShopPage} />
-         <Route  path='/signin' component={SignInAndSignUpPage} />
+class App extends React.Component {
+  constructor() {
+    super();
 
-        </Switch>    
-    </div>
-  );
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+   this.unsubscribeFromAuth= auth.onAuthStateChanged( async user => {
+     createUserProfileDocument(user)
+      console.log(user)
+    })
+  }
+   componentWillUnmount() {
+     this.unsubscribeFromAuth();
+   }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+           <Route exact path='/' component={HomePage} />
+           <Route  path='/shop' component={ShopPage} />
+           <Route  path='/signin' component={SignInAndSignUpPage} />
+  
+          </Switch>    
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,3 +20,11 @@ const config = {
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({ prompt: 'select_account'});
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export const createUserProfileDocument = async (userAuth, additionalData)=> {
+      if (!userAuth) return;
+
+      console.log(firestore.doc('users/gdteryuw566'))
+  }
+
+  export default firebase;
