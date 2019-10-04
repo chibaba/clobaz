2f70c9342813294e56d6e7ab00ff0f6419faca8a
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-10-04 09:33:07 +0100

    createstructuredSelector was added to replace the state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -9,6 +10,7 @@ import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.compone
 import Header from  './Components/header/header.component'; 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action';
+import { selectCurrentUser }  from './redux/user/user.selectors';
 
 
 class App extends React.Component {
@@ -54,8 +56,8 @@ class App extends React.Component {
   }
 
 }
- const mapStateToProps = ({ user }) => ({
-   currentUser: user.currentUser
+ const mapStateToProps = createStructuredSelector ({
+   currentUser: selectCurrentUser
  })
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- src/Components/cart-icon/cart-icon.component.jsx
+++ src/Components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
-
+import {createStructuredSelector } from 'reselect'
 import { toggleCartHidden } from '../../redux/cart/cart.action'
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from  '../../assets/11.2 shopping-bag.svg.svg';
@@ -19,8 +19,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch( toggleCartHidden())
 });
 
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector ({
+  itemCount: selectCartItemsCount 
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
