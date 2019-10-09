61125f7ddbae52a2a6273bfa007dc4f886735822
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-10-09 20:49:35 +0100

    implemented utils to our redux app

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart} from './cart.utils';
+import { addItemToCart, removeItemFromCart} from './cart.utils';
 // import { clearItemFromCart } from './cart.action';
 
 const INITIAL_STATE = {
@@ -23,7 +23,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.REMOVE_ITEM:
             return {
                 ...state,
-                cartItems: 
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             }
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return {
