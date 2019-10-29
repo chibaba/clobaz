941a42d450dc75bc964fdb6ad2aa1473ac5cfeba
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-10-29 22:22:41 +0100

    added collection page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './category.styles.scss'
+
+const CollectionPage = ()=> {
+    console.log(match.params.collectionId)
+    return(
+    <div className='collection-page'>
+        <h2>Collection PAGE</h2>
+    </div>
+)}
+
+export default  CollectionPage
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
-import CollectionOverview from '../../Components/collection-overview/collection-overview.component'
+import { Route } from 'react-router-dom';
 
-const ShopPage = ({ collections }) => (
 
-  
+
+import CollectionOverview from '../../Components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component'
+
+const ShopPage = ({ match }) => {
+    return(
                 <div className="shop-page">
-                 <CollectionOverview />
+                 <Route exact path={`${match.path}`} component={CollectionOverview} />
+                 <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
 
                 </div>
-            );
+            )};
             
         
 export default ShopPage;
