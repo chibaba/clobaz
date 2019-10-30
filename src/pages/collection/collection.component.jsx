06a07792dde4727751ac31523945160762f99865
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-10-31 00:24:02 +0100

    collection component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,10 +8,13 @@ import { selectCollection } from '../../redux/shop/shop.selector'
 import './collection.styles.scss'
 
 const CollectionPage = ({ collection })=> {
-    //console.log(collection)
+  const { title, items } = collection;
     return(
     <div className='collection-page'>
-        <h2>Collection PAGE</h2>
+        <h2 className='title'> { title} </h2>
+        <div className='items'>
+            {items.map(item => <collectionItem key={item.id} item={item} />)}
+        </div>
     </div>
 )}
 
