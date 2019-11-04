2680aabf5cfe4e272ed46c16420be0f6f2867691
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-11-04 01:49:46 +0100

    data normalization

--- src/Components/collection-overview/collection-overview.component.jsx
+++ src/Components/collection-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CollectionPreview  from '../collection-preview/collection-preview';
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 
 import './collection-overview.styles.scss';
 
@@ -18,6 +18,6 @@ const CollectionOverview = ({ collections }) => (
     </div>
 );
     const mapStateToProps = createStructuredSelector({
-        collections: selectCollections
+        collections: selectCollectionsForPreview
     })
 export default connect(mapStateToProps)(CollectionOverview)
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -8,15 +8,21 @@ const COLLECTION_ID_MAP = {
     mens: 5
 }
 
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+);
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
 )
 
 export const selectCollection = collectionUrlParam =>
 createSelector(
     [selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-)
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+    );
