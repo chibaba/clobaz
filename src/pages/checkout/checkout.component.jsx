b22827e9b90a10ab63450f48f4698128556fbdf4
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-11-04 10:41:30 +0100

    stripe payment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../Components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../Components/stripe-button/stripe-button.component'
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 import './checkout.styles.scss';
@@ -35,6 +36,12 @@ const CheckoutPage = ({ cartItems, total}) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *please use the following text credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVv: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 )
 
